43f740572440a1e778dc6de76db7730788447855
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-25 16:46:03 -0600

    add a loader

--- src/car-service.js
+++ src/car-service.js
@@ -3,11 +3,18 @@ import {getNodes} from './template';
 export const loadMoreRequest = () => {
 
   const cars = document.querySelector('main .cars');
+  const loader = document.querySelector('main .first-load');
+
   const endPoint = '/api/latest-deals.json';
 
   return fetch(endPoint)
     .then(response => response.json())
     .then(data => getNodes(...data.cars))
-    .then(nodes => nodes.forEach(node => cars.appendChild(node)));
+    .then(nodes => {
+
+      loader.remove();
+      nodes.forEach(node => cars.appendChild(node));
+
+    });
 
 };
