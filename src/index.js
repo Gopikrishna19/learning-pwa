e8f797d0826f3cb622eb6823a176574a06048182
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-03-02 16:19:20 -0600

    app manifest

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {loadMoreRequest} from './car-service';
 import {registerWorker} from './worker-reg';
 import 'file-loader?name=worker.js!./worker';
+import 'file-loader?name=manifest.json!./manifest.json';
 import './index.scss';
 
 window.services = {
