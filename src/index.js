7d2d62d8108f0b8d49f20c5f15e1ed9a6c1b0b0f
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-03-01 22:06:27 -0600

    remove stale function

--- src/index.js
+++ src/index.js
@@ -3,19 +3,8 @@ import {registerWorker} from './worker-reg';
 import 'file-loader?name=worker.js!./worker';
 import './index.scss';
 
-const refresh = () => {
-
-  if (window.applicationCache) {
-
-    window.applicationCache.update();
-
-  }
-
-};
-
 window.services = {
-  loadMoreRequest,
-  refresh
+  loadMoreRequest
 };
 
 loadMoreRequest();
