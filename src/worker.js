f8dec51c283826b3fc8963e5717cdab82675c1f4
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-03-01 20:42:10 -0600

    clean up on activation

--- src/worker.js
+++ src/worker.js
@@ -14,3 +14,16 @@ self.addEventListener('install', event => {
   );
 
 });
+
+self.addEventListener('activate', event => {
+
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .map(key => key !== CAR_DEALS_CACHE ? caches.delete(key) : null)
+          .filter(promise => promise)
+      ))
+  );
+
+});
